e7045bc95b361a027377e014acba39d0c9ffaa63
gp <p-mishka1@mail.ru>
2024-01-23 12:18:50 +0300

    HelloController

--- src/test/java/gp/nimfa/clientserver/ClientServerApplicationTests.java
+++ src/test/java/gp/nimfa/clientserver/ClientServerApplicationTests.java
@@ -16,12 +16,4 @@ class ClientServerApplicationTests
     {
     }
 
-    @Test
-    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException
-    {
-        GreetClient client = new GreetClient();
-        client.startConnection("127.0.0.1", 6666);
-        String response = client.sendMessage("hello server");
-        assertEquals("hello client", response);
-    }
 }
